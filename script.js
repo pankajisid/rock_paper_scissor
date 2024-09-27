fa5acafecd878e066ee2f213c154b44f4f515eae
pankajisid <p7398622@gmail.com>
2024-09-27 17:41:53 +0530

    Add the UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* produce computer choice based on random number generate by Math.random() function uniformly in (0,1) */
+
 function getComputerChoice(){
     let number =Math.random();
     if(number<=0.3){
@@ -11,7 +12,7 @@ function getComputerChoice(){
         return "scissor";
     }
 }
-/* takes user choice */
+/* takes user choice 
 
 function getHumanChoice(){
     let choice =prompt("Please enter the your choice (rock, paper or scissor) and if you want to quit : type quit :");
@@ -22,41 +23,106 @@ function getHumanChoice(){
 let humanscore=0;
 let computerscore=0;
 
+function displayResult(msg){
+    div.textContent = msg;
+}
 
-function playRound(){
-    const humanChoice=getHumanChoice();
+
+function playRound(str){
+    const humanChoice=str;
     const computerChoice=getComputerChoice();
     if(humanChoice===computerChoice){
-        console.log("Tie");
+        displayResult("Tie");
     }
     else if(humanChoice==="rock" && computerChoice==="scissor"){
+        displayResult("rock beat scissor");
         humanscore++;
-        console.log("rock beat scissor");
     }
     else if(humanChoice==="scissor" && computerChoice==="rock"){
         computerscore++;
-        console.log("rock beat scissor");
+        displayResult("rock beat scissor");
     }
     else if(humanChoice==="rock" && computerChoice==="paper"){
         computerscore++;
-        console.log("paper beat rock");
+        displayResult("paper beat rock");
     }
     else if(humanChoice==="paper" && computerChoice==="rock"){
         humanscore++;
-        console.log("paper beat rock");
+        displayResult("paper beat rock");
     }
     else if(humanChoice==="paper" && computerChoice==="scissor"){
         computerscore++;
-        console.log("scissor beat paper");
+        displayResult("scissor beat paper");
     }
     else if(humanChoice==="scissor" && computerChoice==="paper"){
         humanscore++;
-        console.log("scissor beat paper");
+        displayResult("scissor beat paper");
     }
 }
 
+
+
+const body = document.querySelector("body");
+
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissor = document.createElement("button");
+
+rock.textContent = "rock";
+paper.textContent = "paper";
+scissor.textContent = "scissor";
+
+
+const div = document.createElement("div");
+
+body.insertAdjacentElement('afterend', div);
+
+let flag = true;
+function result(){
+    let score = Math.max(humanscore, computerscore);
+    if(score===5) flag=false;
+    if(humanscore===5 && score ===5){
+        const p = document.createElement("p");
+        p.textContent = `Human Wins with Score 5 and computerscore is ${computerscore}`;
+        div.insertAdjacentElement('afterend', p);
+    }
+    else if(computerscore===5 && score ===5){
+        const p = document.createElement("p");
+        p.textContent = `computer Wins with Score 5 and humanscore is ${humanscore}`;
+        div.insertAdjacentElement('afterend', p);
+    }
+}
+
+
+
+rock.addEventListener('click', () =>{
+    playRound("rock")
+    result();
+});
+body.appendChild(rock);
+
+paper.addEventListener('click', () =>{
+    playRound("paper");
+    result();
+});
+
+body.appendChild(paper);
+
+scissor.addEventListener('click', () =>{
+    playRound("scissor");
+    result();
+});
+
+body.appendChild(scissor);
+
+
+
+
+
+/*
+
 let flag=true;
-/* this functions takes care how long user wants to play */
+
 while(flag){
     playRound();
     if(getHumanChoice().toLowerCase()==="quit"){
@@ -68,3 +134,5 @@ console.log('HumanScore:', humanscore);
 console.log('ComputerScore:',computerscore);
 
 console.log((humanscore > computerscore)?"User wins":"Computer Wins");
+
+*/
